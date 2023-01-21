f5a5be41f17a25079e5c122541154ac236b75975
Asmenys <76791232+Asmenys@users.noreply.github.com>
2023-01-21 23:10:18 +0200

    removed all methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,38 +1,12 @@
 class StaticPagesController < ApplicationController
-  def index
-    @photo_url = nil
-    set_photo_url
-  end
+  def index = @photo_urls ||= get_photo_urls
 
   private
-  def set_photo_url
-    photo_url = nil
-    user_id = get_id_from_params
-    unless user_id.nil?
-     photo_url = get_photo_url_from(get_random_user_photo_info_from(user_id))
-    end
-    @photo_url = photo_url
-  end
+  def get_photo_urls
 
-  def get_id_from_params
-    id = nil
-    if params.key?(:id) and params[:id] != ""
-      id = params[:id]
-    end
-    id
   end
-
-  def get_random_user_photo_info_from(user_id)
-    photo_array = @@flickr.photos.search(:user_id => "#{user_id}")
-    photo_array[get_random_photo_index(photo_array.length)]
-  end
-
-  def get_photo_url_from(photo_info)
-    "https://live.staticflickr.com/#{photo_info.server}/#{photo_info.id}_#{photo_info.secret}.jpg"
-  end
-
-  def get_random_photo_index(photo_array_length)
-    rand(0..photo_array_length-1)
+  
+  def query_params
   end
 
 end
