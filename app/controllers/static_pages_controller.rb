055ea8f675c13da2d0260c4ac551554de956921b
Asmenys <76791232+Asmenys@users.noreply.github.com>
2023-01-22 19:51:56 +0200

    query parameter selection menu

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,12 @@
 class StaticPagesController < ApplicationController
-  def index = @photo_urls ||= get_photo_urls
+  def index = @photo_urls ||= get_photo_array
 
   private
-  def get_photo_urls
-
+  def get_photo_array
+    #photo_array = @@flickr.photos.search(query_params)
   end
-  
+
   def query_params
+    parameters.permit(:query, :query_parameters);
   end
-
 end
--- app/javascript/controllers/query_controller.js
+++ app/javascript/controllers/query_controller.js
@@ -1,14 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["switch", "parameters"]
-
+    static targets = ["parameterToggle", "parameters", "paramSlide", "slideSwitch"]
+    initialize() {
+        this.slideIndex = 0
+    }
     display() {
         if (this.parametersTarget.hidden) {
-            this.parametersTarget.hidden = false;
+            this.parametersTarget.hidden = false
         }
         else {
-            this.parametersTarget.hidden = true;
+            this.parametersTarget.hidden = true
         }
+    }
+
+    updateSlideState(event) {
+        var switchIndex = event.currentTarget.attributes["data-query-variable"].value
+        this.resetActiveStates(this.slideIndex)
+        this.showSlide(switchIndex)
+        this.updateSwitch(switchIndex)
+        this.slideIndex = switchIndex
+    }
+    resetActiveStates(switchIndex) {
+        this.slideSwitchTargets[switchIndex].className = "w-10 border-black border-solid border-2"
+        this.paramSlideTargets[switchIndex].hidden = true
+    }
+    showSlide(slideIndex) {
+        this.paramSlideTargets[slideIndex].hidden = false
+    }
+    updateSwitch(switchIndex)
+    {
+        this.slideSwitchTargets[switchIndex].className = "w-10 border-red-900 border-solid border-2"
     }
 }
