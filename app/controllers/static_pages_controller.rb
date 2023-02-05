255f8aa8c63f4b36cf0f978fe83a6871c14647f2
Asmenys <76791232+Asmenys@users.noreply.github.com>
2023-02-05 17:33:14 +0200

    splits tags on commas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,7 @@ class StaticPagesController < ApplicationController
   private
 
   def get_photo_list_from_flickr
-    @@flickr.photos.search :tags => query_params["tags"], :tag_mode => query_params["tag_mode"], :content_type => query_params["content_type"]
+    @@flickr.photos.search :tags => query_params["tags"].split(','), :tag_mode => query_params["tag_mode"], :content_type => query_params["content_type"]
   end
 
   def photo_list_to_urls(photo_list)
