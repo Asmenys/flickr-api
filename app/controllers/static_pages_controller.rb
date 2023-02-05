ae2216df2e1a255d3ff1412ddb9990be20e866d3
Asmenys <76791232+Asmenys@users.noreply.github.com>
2023-02-05 16:47:10 +0200

    photo retrieval from flickr based on form information

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,22 @@
 class StaticPagesController < ApplicationController
   def index
+    @photo_list = nil
+    if params[:commit] == "search"
+      @photo_list = get_photo_list_from_flickr
+    end
   end
 
   private
-  def get_photo_array
-    #photo_array = @@flickr.photos.search(query_params)
+
+  def get_photo_list_from_flickr
+    @@flickr.photos.search :tags => query_params["tags"], :tag_mode => query_params["tag_mode"], :content_type => query_params["content_type"]
   end
 
+  def photo_list_to_urls(photo_list)
+    url_list = []
+  end
+ 
   def query_params
-    params.require(:query).permit(
-      :tags, query_parameters: [:tag_mode, :content_type]
-    )
+    params.require(:query).permit(:tags, :tag_mode, :content_type)
   end
 end
